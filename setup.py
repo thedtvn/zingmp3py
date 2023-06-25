0e84c59508db5dd1bb457da51b1060490bc8cf2b
The DT <duongtuan30306@gmail.com>
2023-06-25 22:29:06 +0700

    update ver

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-with open("README.md", "r") as f:
+with open("./README.md", "r") as f:
    ld = f.read()
 
 setup(
    name='zingmp3py',
    version="0.3.2",
    long_description=ld,
+   long_description_content_type='text/markdown',
    description='A light weight Python library for the ZingMp3 API',
    author='The DT',
    author_email='duongtuan30306@gmail.com',
